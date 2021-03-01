a2bb73424a1e3d1d1d6e53cb901a0ee735ea86b8
Saikrishna emmadishetty <60013018+Saikrishna1545@users.noreply.github.com>
2021-03-01 10:14:23 -0600

    Update index.js
    updated index.js

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ const express = require('express')
 const path = require('path')
 const engines = require('consolidate')
 const app = express();
-
 function onRequest(request, response) {
     response.writeHead(200, {
         'Content-Type': 'text/plain'
     });
+    
     response.write('GDP-2 Project ');
     response.write('Team Members:');
-    response.write('GD and Sai Krishna');
+    response.write('GD Prasad and Sai Krishna Emmadishetty');
     response.end();
 }
 app.set('views', path.join(__dirname, './views'))
 app.set('view engine', 'ejs')
 app.engine('ejs', engines.ejs)
-http.createServer(onRequest).listen(3000);
+http.createServer(onRequest).listen(3000); // port 3000 as used
