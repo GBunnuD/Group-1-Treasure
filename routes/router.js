1913d5d1109b0d7ab7cf16b49c0429a7360b84d5
GD-Prasad <g.d.prasad004@gmail.com>
2021-03-01 09:29:38 -0600

    added routes file.

---
+++ routes/router.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const md5 = require('md5')
+const router = express.Router()
+router.use(bodyParser.urlencoded({ extended: true }));
+const sqldb = require('.././database')
+const axios = require('axios')
+var jsonParser = bodyParser.json()
+
+router.get('/', (req, res) => {
+    res.render('./index', { title: 'index' })
+})
