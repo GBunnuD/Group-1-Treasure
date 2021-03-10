b05a6e9409b8adea14ac554083d722d0ac84496a
GD-Prasad <59986885+GD-Prasad@users.noreply.github.com>
2021-03-10 02:17:21 -0600

    Deleted commented code.

--- index.js
+++ index.js
@@ -6,16 +6,7 @@ const app = express();
 const md5 = require('md5');
 const bodyParser = require('body-parser');
 
-// function onRequest(request, response) {
-//     response.writeHead(200, {
-//         'Content-Type': 'text/plain'
-//     });
 
-//     response.write('GDP-2 Project ');
-//     response.write('Team Members:');
-//     response.write('GD Prasad and Sai Krishna Emmadishetty');
-//     response.end();
-// }
 app.set('view engine', 'ejs')
 app.engine('ejs', engines.ejs)
 app.set('views', path.join(__dirname, './views'))
@@ -25,6 +16,6 @@ app.engine('ejs', engines.ejs)
 const router = require('./routes/router')
 app.use('/', router)
 app.use(bodyParser.json({ type: "application/*+json" }));
-// http.createServer().listen(3000); // port 3000 as used
+
 
 app.listen(3000)
