845537aa2e57968ee77e84518c15726b9556b418
GD-Prasad <g.d.prasad004@gmail.com>
2021-03-10 00:11:51 -0600

    Added routes and .ejs to the project.

--- index.js
+++ index.js
@@ -3,17 +3,28 @@ const express = require('express')
 const path = require('path')
 const engines = require('consolidate')
 const app = express();
-function onRequest(request, response) {
-    response.writeHead(200, {
-        'Content-Type': 'text/plain'
-    });
-    
-    response.write('GDP-2 Project ');
-    response.write('Team Members:');
-    response.write('GD Prasad and Sai Krishna Emmadishetty');
-    response.end();
-}
+const md5 = require('md5');
+const bodyParser = require('body-parser');
+
+// function onRequest(request, response) {
+//     response.writeHead(200, {
+//         'Content-Type': 'text/plain'
+//     });
+
+//     response.write('GDP-2 Project ');
+//     response.write('Team Members:');
+//     response.write('GD Prasad and Sai Krishna Emmadishetty');
+//     response.end();
+// }
+app.set('view engine', 'ejs')
+app.engine('ejs', engines.ejs)
 app.set('views', path.join(__dirname, './views'))
 app.set('view engine', 'ejs')
 app.engine('ejs', engines.ejs)
-http.createServer(onRequest).listen(3000); // port 3000 as used
+
+const router = require('./routes/router')
+app.use('/', router)
+app.use(bodyParser.json({ type: "application/*+json" }));
+// http.createServer().listen(3000); // port 3000 as used
+
+app.listen(3000)
--- routes/router.js
+++ routes/router.js
@@ -3,10 +3,12 @@ const bodyParser = require('body-parser');
 const md5 = require('md5')
 const router = express.Router()
 router.use(bodyParser.urlencoded({ extended: true }));
-const sqldb = require('.././database')
-const axios = require('axios')
+// const sqldb = require('.././database')
+// const axios = require('axios')
 var jsonParser = bodyParser.json()
 
 router.get('/', (req, res) => {
-    res.render('./index', { title: 'index' })
+    res.render('../views/index', { title: 'index' })
 })
+
+module.exports = router;
