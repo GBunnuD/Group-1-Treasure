34d167877da242ad0e1cf36bcf46f4561a2593ad
SaiKrishna1545 <s538340@nwmissouri.edu>
2021-03-17 09:14:40 -0500

    updated routes

--- routes/router.js
+++ routes/router.js
@@ -2,10 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser');
 const md5 = require('md5')
 const router = express.Router()
-router.use(bodyParser.urlencoded({ extended: true }));
-var jsonParser = bodyParser.json()
-require('dotenv').config();
-const locationController = require('../controllers/locationcontroller')
+
 
 
 router.get('/', (req, res,next) => {
