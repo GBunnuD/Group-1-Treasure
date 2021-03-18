1ae7fae429bc3ffc8cf5346f0abb7f2510f67870
GD-Prasad <59986885+GD-Prasad@users.noreply.github.com>
2021-03-18 13:06:21 -0500

    Trying to fix Heroku errors.

--- index.js
+++ index.js
@@ -1,41 +1,40 @@
-// const dotenv = require('dotenv');
+const dotenv = require('dotenv');
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
 const helmet = require('helmet');
-var env = require('dotenv').config()
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const engines = require('consolidate');
 const mongoose = require("mongoose");
+const engines = require('consolidate');
 const expressLayouts = require('express-ejs-layouts');
+require('dotenv').config({ path: 'ENV_FILENAME' });
 
-// const LOG = require('./utils/logger');
+
 
 // env variables
 const hostname = process.env.HOSTNAME;
 const isProduction = process.env.NODE_ENV === 'production';
-// LOG.info('Environment isProduction = ', isProduction);
 
-// create an Express app
 const app = express();
-require('dotenv').config({path:'config.env'});
-
-
 
 var http = require('http');
 
+const md5 = require('md5');
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 3000
 
 
-// const vars = env.config({ path: '.env' });
-// if (vars.error) {
-//   throw vars.error;
-// }
-//
+const vars = dotenv.config({ path: '.env' });
+if (vars.error) {
+    throw vars.error;
+}
+
 app.use(express.static('public'));
 
 app.use(helmet());
 
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.engine('ejs', engines.ejs);
@@ -46,6 +45,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(expressLayouts);
+
+// load seed data
+require('./utils/seeder.js')(app);
+
+// Use Express middleware to configure routing
+const routing = require('./routes/router.js');
 
 app.set('view engine', 'ejs')
 app.engine('ejs', engines.ejs)
@@ -58,15 +64,13 @@ app.use('/', router)
 app.use(bodyParser.json({ type: "application/*+json" }));
 app.use(express.static(__dirname + '/public/'));
 // app.listen(port, () => {
-//     console.log(`Example app listening at http://localhost:${port}`, app.settings.env)
+//     console.log(`Example app listening at http://localhost:${port}`)
 // })
+mongoose.connect(process.env.ATLAS_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then((res) => {
+    app.listen(3000, function() {
+        return "Connected to Database"
 
-
-mongoose.connect(process.env.ATLAS_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then((res) =>{
-  app.listen(3000, function () {
-  return "Connected to Database"
-  
-})
+    })
 }).catch((e) => {
- console.log(e,"--error")
+    console.log(e, "--error")
 })
