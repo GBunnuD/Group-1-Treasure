481dc7d855b3ed5bd64194f9e95fd4fdf43b5870
GD-Prasad <59986885+GD-Prasad@users.noreply.github.com>
2021-04-10 21:19:28 -0500

    Completed All Functionalities

--- routes/router.js
+++ routes/router.js
@@ -7,22 +7,19 @@ require('dotenv').config();
 const services = require('../render');
 const locationController = require('../controllers/locationcontroller')
 
-router.get('/', (req, res, next) => {
-    res.render('../views/index', { title: 'index' })
-})
+
 
 router.get('/GD', (req, res, next) => {
     res.render('../views/GD', { title: 'GD' })
 })
-router.get('/lat', (req, res, next) => {
-    res.render('../views/lat', { title: 'lat' })
-})
+
 
 router.get('/aboutme', (req, res, next) => {
     res.render('../views/aboutme', { title: 'aboutme' })
 })
 
-router.get('/random', locationController.getHomePage)
+router.get('/', locationController.getHomePage);
+router.get('/index', locationController.getHomePage);
 router.get('/location/display', services.displays);
 router.get('/location/create', services.addlocation);
 router.get('/location/editview/:locationId', services.editLocation);
