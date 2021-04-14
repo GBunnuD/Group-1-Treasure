ae59439ece3e15aa9fa814aec34b080691cd2bea
GD-Prasad <59986885+GD-Prasad@users.noreply.github.com>
2021-04-14 09:38:26 -0500

    Removed Location Name on 1st Box

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -7,7 +7,7 @@ let incrementer = 1;
 
 function main() {
     console.log('Page is fully loaded');
-    console.log(questLocationLat);
+    // console.log(questLocationLat);
     if (incrementer == 1) {
         document.getElementById("ready").innerHTML = "Click to start playing";
         document.getElementById("ready1").innerHTML = " ";
@@ -47,7 +47,7 @@ function display() {
     document.getElementById("ready1").innerHTML = " Start playing the game.";
     let utterance = new SpeechSynthesisUtterance(`The treasure location is ready start playing the game.`);
     speechSynthesis.speak(utterance);
-    console.log(incrementer)
+    // console.log(incrementer)
     document.getElementById("lname").innerHTML = questLocationName;
     incrementer = 1;
 
@@ -71,34 +71,34 @@ async function onClickSquareBox2() {
         const locText = await getLocation();
 
         currentlat = locText.coords.latitude;
-        console.log("============>clat", currentlat)
+        // console.log("============>clat", currentlat)
         document.getElementById("device-lat").innerHTML = currentlat.toFixed(9);
         currentlon = locText.coords.longitude;
-        console.log(currentlon)
+        // console.log(currentlon)
         document.getElementById("device-long").innerHTML = currentlon.toFixed(9);
-        console.log("===============before is inside");
-        console.log(questLocationLat);
+        // console.log("===============before is inside");
+        // console.log(questLocationLat);
 
 
 
         if (isInside() == true) {
-            console.log("==========> inside inside")
+            // console.log("==========> inside inside")
             document.getElementById("target").innerHTML = questLocationName;
             let utterance = new SpeechSynthesisUtterance(`Congratulations!, You found location ${questLocationName}`);
             speechSynthesis.speak(utterance);
-            console.log(questLocationLat);
+            // console.log(questLocationLat);
             error = false;
         };
 
         if (error) {
-            console.log("error is here")
+            // console.log("error is here")
             document.getElementById("error").innerHTML = "Sorry,You're not near to the treasure";
             let utterance = new SpeechSynthesisUtterance("Sorry,You're not near to the treasure");
             speechSynthesis.speak(utterance);
         } else {
             document.getElementById("target1").innerHTML = "";
         }
-        console.log(incrementer, "===================>")
+        // console.log(incrementer, "===================>")
 
     } else {
         document.getElementById("target").innerHTML = "First click on Box 2";
@@ -109,10 +109,10 @@ async function onClickSquareBox2() {
 function isInside(questLocationLat, questLocationLong) {
     var questLocationLat = document.getElementById("llat").getAttribute("value");
     var questLocationLong = document.getElementById("llong").getAttribute("value");
-    console.log(questLocationLat);
+    // console.log(questLocationLat);
     let distance = distanceBetweenLocations(currentlat, currentlon, questLocationLat, questLocationLong);
-    console.log("distance: " + distance);
-    console.log("quest lat " + questLocationLat);
+    // console.log("distance: " + distance);
+    // console.log("quest lat " + questLocationLat);
 
 
     if (distance < 30) {
@@ -124,13 +124,13 @@ function isInside(questLocationLat, questLocationLong) {
 
 function distanceBetweenLocations(currentlat, currentlon, questLocatiionLat, questLocationLong) {
     var p = 0.017453292519943295;
-    console.log(currentlat, currentlon, questLocatiionLat, questLocationLong)
+    // console.log(currentlat, currentlon, questLocatiionLat, questLocationLong)
     var a = 0.5 - Math.cos((questLocatiionLat - currentlat) * p) / 2 +
         Math.cos(currentlat * p) * Math.cos(questLocatiionLat * p) *
         (1 - Math.cos((questLocationLong - currentlon) * p)) / 2;
-    console.log("================>");
+    // console.log("================>");
     var result = 12742 * Math.asin(Math.sqrt(a));
-    console.log(result)
+    // console.log(result)
 
     return result;
 
